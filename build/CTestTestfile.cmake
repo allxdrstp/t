69a1fcc1af0b1be6875d8ba40e04dbd18a4e2821
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-06-19 20:07:41 +0300

    Update CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,8 +1,8 @@
-CMake generated Testfile for 
-# Source directory: /home/allxdrstp/workspace/fun
-# Build directory: /home/allxdrstp/workspace/fun/build
+# CMake generated Testfile for 
+# Source directory: /home/alexandra/allxdrstp/workspace/projects/fun
+# Build directory: /home/alexandra/allxdrstp/workspace/projects/fun/_build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(funExampleTests "/home/allxdrstp/workspace/fun/build/funExampleTests")
-set_tests_properties(funExampleTests PROPERTIES  _BACKTRACE_TRIPLES "/home/allxdrstp/workspace/fun/CMakeLists.txt;17;add_test;/home/allxdrstp/workspace/fun/CMakeLists.txt;0;")
+add_test(funExampleTests "/home/alexandra/allxdrstp/workspace/projects/fun/_build/funExampleTests")
+set_tests_properties(funExampleTests PROPERTIES  _BACKTRACE_TRIPLES "/home/alexandra/allxdrstp/workspace/projects/fun/CMakeLists.txt;16;add_test;/home/alexandra/allxdrstp/workspace/projects/fun/CMakeLists.txt;0;")
