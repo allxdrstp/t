b62bc75f779641c4440129202c4df9343304e55a
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-06-19 17:59:57 +0300

    Update fun.cpp

--- fun.cpp
+++ fun.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
 #include <string>
+#include "fun.h" 
 
-
-class B {
-public:
-    std::string someFunc(const std::string& word1, const std::string& word2) {
-        return word1 + " " + word2 + ".";
-    }
-};
-
-class A {
-public:
-    A() { b = new B; }
-    ~A() { delete b; }
-
-    std::string f1(const std::string& word1, const std::string& word2) {
-        return b->someFunc(word1, word2);
-    }
-
-private:
-    B* b;
-};
 
 int main() {
     A a;
